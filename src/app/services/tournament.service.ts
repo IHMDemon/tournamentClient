442af0fc7527c8a4deba02658c38778bdd908f81
Chad Barcelo <chadbarcelo@outlook.com>
2018-08-03 16:03:56 -0400

    import modules

--- src/app/services/tournament.service.ts
+++ src/app/services/tournament.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TournamentService {
 
-  constructor() { }
+  constructor(private myhttp: Http) { }
+
+  handleError(e) {
+    console.log(e);
+     return Observable.throw(e.json().message);
+  }
+
+  createTournament(theTournament) {
+    return this.myhttp.post(`http://localhost:3000/api/tournament/create`, theTournament, {withCredentials: true})
+      .map(res => res.json())
+  }
+
+  deleteTeam(){
+
+  }
+
+
 }
