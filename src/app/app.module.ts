82fdaf4be1974e4101dad9aca04867bd97bd95e1
Chad Barcelo <chadbarcelo@outlook.com>
2018-08-03 11:03:01 -0400

    tournament components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TournamentBracketComponent } from './tournament-bracket/tournament-bracket.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { TournamentCreateComponent } from './tournament/tournament-create/tournament-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TournamentBracketComponent
+    TournamentBracketComponent,
+    TournamentComponent,
+    TournamentCreateComponent
   ],
   imports: [
     BrowserModule
