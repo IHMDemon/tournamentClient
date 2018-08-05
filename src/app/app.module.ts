a482863f4d9d23a81d215ea0ee6ced29bee7a781
Chad Barcelo <chadbarcelo@outlook.com>
2018-08-04 23:00:48 -0400

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,26 @@ import { TournamentService } from './services/tournament.service';
 import { TournamentBracketComponent } from './tournament/tournament-bracket/tournament-bracket.component';
 import { TeamCreationComponent } from './teams/team-creation/team-creation.component';
 import { UserCreationComponent } from './users/user-creation/user-creation.component';
+import { MainMenuComponent } from './main/main-menu/main-menu.component';
+
 const routes: Routes = [
-  { path: 'team/:id',  component: TeamCreationComponent},
-  { path: 'tournament', component: TournamentCreateComponent },
+  { path: 'team/:id',  
+    component: TeamCreationComponent 
+  },
+  { path: 'tournament', 
+    component: TournamentComponent 
+  },
+  { path: 'tournament/create', 
+    component: TournamentCreateComponent 
+  },
+  { path: '', 
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  { path: '**', 
+    redirectTo: '/',
+    pathMatch: 'full' 
+  }
   ];
 
 // sample inside route array   {path: 'api/tasks/delete/:id', component: TodoListComponent}
@@ -27,6 +44,7 @@ const routes: Routes = [
     TournamentBracketComponent,
     TeamCreationComponent,
     UserCreationComponent,
+    MainMenuComponent,
   ],
   imports: [
     BrowserModule,
