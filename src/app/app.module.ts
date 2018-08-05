375abad0f87da223a5d6335c6b74b9baa21abcc8
Giannini Charles <12gcharles@gmail.com>
2018-08-04 21:22:27 -0400

    Resolved Git disaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from "@angular/router";
@@ -16,18 +15,10 @@ import { UserService } from './services/user.service';
 
 
 
-=======
-import {FormsModule} from '@angular/forms'
-import { RouterModule, Routes} from "@angular/router"
-import { HttpModule } from '../../node_modules/@angular/http';
-import { UserService } from './services/user.service';
-
->>>>>>> 91acd0f4469db625e4c6449b138ec3bf4fe18d89
 import { AppComponent } from './app.component';
 import { TournamentComponent } from './tournament/tournament.component';
 import { TournamentCreateComponent } from './tournament/tournament-create/tournament-create.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-<<<<<<< HEAD
 import { TournamentBracketComponent } from './tournament/tournament-bracket/tournament-bracket.component';
 import { TeamCreationComponent } from './teams/team-creation/team-creation.component';
 import { UserCreationComponent } from './users/user-creation/user-creation.component'
@@ -36,20 +27,6 @@ const routes: Routes = [
   { path: 'tournament/create', component: TournamentCreateComponent },
   { path: 'signup', component: UserCreationComponent}
   ];
-=======
-import { UsersComponentComponent } from './users/users-component/users-component.component';
-
-const routes: Routes = [
-
-  {path: 'signup', component: UsersComponentComponent}
-
-
-
-
-]
-
-
->>>>>>> 91acd0f4469db625e4c6449b138ec3bf4fe18d89
 
 // sample inside route array   {path: 'api/tasks/delete/:id', component: TodoListComponent}
   
@@ -59,30 +36,20 @@ const routes: Routes = [
     NavBarComponent,
     TournamentComponent,
     TournamentCreateComponent,
-<<<<<<< HEAD
     TournamentBracketComponent,
     TeamCreationComponent,
     UserCreationComponent,
-=======
     NavBarComponent,
-    UsersComponentComponent,
->>>>>>> 91acd0f4469db625e4c6449b138ec3bf4fe18d89
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-<<<<<<< HEAD
     RouterModule,
     RouterModule.forRoot(routes)
   ],
   providers: [TournamentService, UserService],
 
-=======
-    RouterModule.forRoot(routes)
-  ],
-  providers: [UserService],
->>>>>>> 91acd0f4469db625e4c6449b138ec3bf4fe18d89
   bootstrap: [AppComponent]
 })
 export class AppModule { }
