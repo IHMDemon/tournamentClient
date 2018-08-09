89078882656d80f39e6215810b2476f6e7f613fa
Chad Barcelo <chadbarcelo@outlook.com>
2018-08-09 08:15:30 -0400

    brackets fluid and in the right order

--- src/app/teams/all-teams/all-teams.component.ts
+++ src/app/teams/all-teams/all-teams.component.ts
@@ -12,8 +12,6 @@ import { Router, Route} from '@angular/router';
 })
 export class AllTeamsComponent implements OnInit {
 
-  firstTeam:Array<any> = [];
-  secondTeam:Array<any> = [];
   theLoggedInUser: any = {}
   theError: any;
   allTheTeams: Array<any>;
@@ -27,38 +25,6 @@ export class AllTeamsComponent implements OnInit {
     public activatedRoute: ActivatedRoute,
     public tournamentService: TournamentService,
   ) { }
-
-    
-    // becomes a worthless piece of shit code if
-    // I get express working which I have no idea 
-    // how I fucking do it note to self work on 
-    // express first fuck the angular side and 
-    // fuck it with a straw
-    //
-    //
-    // assignAllTeamsToBracket(){
-    //   this.TeamService.getteams()
-    //   .subscribe((res)=>{
-    //     this.teamBracketBitch = res;
-    //     if(this.teamBracketBitch.length%2 === 0){
-    //     for(let x=0; x <= this.teamBracketBitch.length-1;x++){
-    //       if(x%2 === 0){
-    //         this.secondTeam.unshift(this.teamBracketBitch[x]);
-    //         }
-    //         else if (x%2 === 1){
-    //           this.firstTeam.unshift(this.teamBracketBitch[x]);
-    //         }
-    //         else {console.log("I'm inside all-teams component ts");}
-    //     }
-    //     this.theBracket = {
-    //       firstTeam: this.firstTeam,
-    //       secondTeam: this.secondTeam
-    //     }
-    //     console.log(this.theBracket);
-    //     return this.theBracket;
-    //   } else {console.log("YOU NEED ONE MORE TEAM BEFORE WE CAN START")}
-    //   })
-    // }
 
     getAllTheTeams(){
       this.TeamService.getteams()
